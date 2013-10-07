68931ecb4762cc72a010c7ae616ebaae736ab394
Phil Laird <lucaboq@gmail.com>
2013-10-07 12:43:55 -0700

    Update test app to work with 3.x

--- test-app/Resources/app.js
+++ test-app/Resources/app.js
@@ -1,18 +1,19 @@
 /*
  * Single Window Application Template:
  * A basic starting point for your application.  Mostly a blank canvas.
- * 
+ *
  * In app.js, we generally take care of a few things:
  * - Bootstrap the application with any data we need
  * - Check for dependencies like device type, platform version or network connection
  * - Require and open our top-level UI component
- *  
+ *
  */
 
 //bootstrap and check dependencies
 if (Ti.version < 1.8 ) {
-	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');	  	
+	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');
 }
+
 // This is a single context application with multiple windows in a stack
 (function() {
 	//render appropriate components based on the platform and form factor
@@ -20,10 +21,11 @@ if (Ti.version < 1.8 ) {
 		version = Ti.Platform.version,
 		height = Ti.Platform.displayCaps.platformHeight,
 		width = Ti.Platform.displayCaps.platformWidth;
-	
+
 	//considering tablet to have one dimension over 900px - this is imperfect, so you should feel free to decide
 	//yourself what you consider a tablet form factor for android
 	var isTablet = osname === 'ipad' || (osname === 'android' && (width > 899 || height > 899));
+
 	var Window;
 	if (isTablet) {
 		Window = require('ui/tablet/ApplicationWindow');
@@ -40,7 +42,7 @@ if (Ti.version < 1.8 ) {
 	}
 	var window = new Window();
 	window.open();
-	
+
 	var tftModule = require('com.tapfortap.ti');
     Ti.API.info("module is => " + tftModule);
 
@@ -53,9 +55,9 @@ if (Ti.version < 1.8 ) {
 		top: 0,
 		left: 0
 	});
-	
+
 	Ti.API.info("adView is => " + adView);
-	
+
 	adView.addEventListener("tap", function(d) {
 		Ti.API.info("Banner was tapped");
 	});
@@ -67,7 +69,7 @@ if (Ti.version < 1.8 ) {
 	adView.addEventListener("error", function(d) {
 		Ti.API.info("Failed to receive banner because ", JSON.stringify(d, null, 2));
 	});
-	
+
 	window.add(adView);
 
 	var showAppWallButton = Ti.UI.createButton({
@@ -128,9 +130,4 @@ if (Ti.version < 1.8 ) {
 	});
 	window.add(showInterstitialButton);
 
-
 })();
-
-
-
-
